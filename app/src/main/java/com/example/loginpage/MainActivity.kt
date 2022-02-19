40eaf5fc2b53eff1c1856910eed5976d52fc469b
iamhakeem <hakeemaro20@gmail.com>
2022-02-19 12:37:36 +0100

    fixed

--- app/src/main/java/com/example/loginpage/MainActivity.kt
+++ app/src/main/java/com/example/loginpage/MainActivity.kt
@@ -5,13 +5,19 @@ import android.os.Bundle
 import android.view.View
 import android.widget.*
 import android.widget.ProgressBar
+import kotlinx.android.synthetic.main.activity_main.*
 
 
 
 
-class MainActivity : AppCompatActivity() {
+
+class   MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnlogin.setOnclickListiner {
+
+        }
     }
 }
